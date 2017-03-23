c6fe4678874df23c691dd2909db1ba3f896f9f36
Natasha Clark <Natasharclark@gmail.com>
2017-03-23 11:49:10 -0400

    scraper scraping

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const cheerio = require("cheerio")
+const request = require("request")
+const fs  = require('fs')
+
+const app = express()
+
+app.get('/', function (req, res){
+
+  url = 'https://ucr.fbi.gov/crime-in-the-u.s/2015/crime-in-the-u.s.-2015/tables/table-1'
+
+  //app code that makes it work
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html)
+      var title, release, rating
+      var json = { population: ""}
+
+      $('#cell51').filter(function(){
+        var data = $(this)
+        population = data.html()
+
+        console.log(population)
+
+        json.population = population;
+        res.send(population)
+      })
+    }
+  })
+})
+
+app.listen('4000')
+console.log('Scraping faces on 4k')
+
+exports = module.exports = app
